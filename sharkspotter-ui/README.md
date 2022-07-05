a75dac60d36b98b249e4dc82a9486ebc6cb51717
thaborach <thaborach@bbd.co.za>
2022-07-05 19:00:48 +0200

    init forntend

---
+++ sharkspotter-ui/README.md
@@ -0,0 +1,19 @@
+# SharkSpotter Frontend
+
+## Getting Started
+
+In the project directory, you can run
+
+1. Install the required modules
+
+    ```bash
+    npm i
+    ```
+
+2. Start the app in development mode:
+
+    ```bash
+    npm start
+    ```
+
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
---
+++ sharkspotter-ui/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+import Header from './components/header/header';
+
+function App() {
+  return (
+    <> 
+      <Header/>
+      <main className='content'>
+        <p>Content Goes Here</p>
+        <p>More content...</p>
+        <p>More content...</p>
+        <p>More content...</p>
+      </main>
+    </>
+  );
+}
+
+export default App;
