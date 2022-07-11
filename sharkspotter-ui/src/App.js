ac97041a971ef1ec730e651d691c0cafc3e0b142
thaborach <thaborach@bbd.co.za>
2022-07-11 21:56:01 +0200

    create header

--- sharkspotter-ui/src/App.js
+++ sharkspotter-ui/src/App.js
@@ -1,16 +1,14 @@
 import './App.css';
 
 import Header from './components/header/header';
+// import Home from './pages/home/home';
 
 function App() {
   return (
     <> 
       <Header/>
       <main className='content'>
-        <p>Content Goes Here</p>
-        <p>More content...</p>
-        <p>More content...</p>
-        <p>More content...</p>
+        {/* <Home /> */}
       </main>
     </>
   );
