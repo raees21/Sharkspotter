6b78c5a886d67f80b4b9f3a571a92defb3b55c51
raees21 <raeesaziz786@gmail.com>
2022-07-12 21:13:33 +0200

    Secrets Amazon

--- Sharkspotter-Backend/Program.cs
+++ Sharkspotter-Backend/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Sharkspotter_Backend.Data;
-
+using Sharkspotter_Backend.SecretsManager;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,9 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 builder.Services.AddDbContext<DataContext>(
-    options => options.UseSqlServer(builder.Configuration.GetConnectionString("SharkspotterDB")));
+    options => options.UseSqlServer(GetSecretsClass.GetSecret()));
 
 builder.Services.AddScoped<UserService>();
 builder.Services.AddScoped<BeachService>();
