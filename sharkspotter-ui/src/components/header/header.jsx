5f8dc143083f2da557908731e63fbcce319e3e19
thaborach <thaborach@bbd.co.za>
2022-07-12 22:02:57 +0200

    add link to header logo

--- sharkspotter-ui/src/components/header/header.jsx
+++ sharkspotter-ui/src/components/header/header.jsx
@@ -5,10 +5,14 @@ import Button from '../button/button';
 
 import "./header.css";
 
+import { Link } from 'react-router-dom';
+
 function Header() {
     return (
         <header className="App-header">
-            <h1 className="logo">Sharkspotter</h1>
+            <h1 className="logo">
+                <Link to="/" className="logo-link">Sharkspotter</Link>
+            </h1>
             <section className="button-group">
                 <Button link_to="/testing" type="primary" text="Report a Sighting" />
             </section>
