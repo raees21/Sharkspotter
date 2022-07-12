e899381460c08493d01296ac8ca9e6df36c92666
Emanuel Maluleke <emanuelm@bbd.co.za>
2022-07-12 10:35:52 +0200

    add description in beaches

--- Sharkspotter-Backend/Models/Beach.cs
+++ Sharkspotter-Backend/Models/Beach.cs
@@ -6,14 +6,16 @@
         public decimal latitude { get; set; }
         public decimal longitude { get; set; }
         public string beach_name { get; set; }
+        public string description {get;set;}
 
         public Beach() { }
-        public Beach(int beachid, decimal latitude, decimal longitude, string beach_name)
+        public Beach(int beachid, decimal latitude, decimal longitude, string beach_name, string description)
         {
             this.beachid = beachid;
             this.latitude = latitude;
             this.longitude = longitude;
             this.beach_name = beach_name;
+            this.description = description;
 
         }
 
@@ -23,6 +25,7 @@
             this.longitude = beach.longitude;
             this.beach_name = beach.beach_name;
             this.latitude = beach.latitude;
+            this.description = beach.description;
         }
 
     }
