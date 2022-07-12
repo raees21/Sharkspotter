2ad6feffc3f71f15c47a162ee8cd1bfb87e97ea1
BoitumeloMashiane <boitumelo.bibo@gmail.com>
2022-07-12 22:54:46 +0200

    packages for the form page

--- sharkspotter-ui/src/App.js
+++ sharkspotter-ui/src/App.js
@@ -6,6 +6,7 @@ import Home from './pages/home/home'
 import Testing from './pages/testing/testing';
 import Error from './pages/error/error';
 import Header from './components/header/header';
+import Spotting from './pages/spotting/spotting';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Routes>
             <Route index  element={<Home />} />
             <Route path="/testing" element={<Testing/>} />
+            <Route path="/spotting" element={<Spotting/>} />
             <Route path="*" element={<Error/>} />
         </Routes>
       </main>
