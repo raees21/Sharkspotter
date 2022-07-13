49c781dc33cb4ad6df04ead3e99532d008c38eba
Emanuel Maluleke <emanuelm@bbd.co.za>
2022-07-13 17:21:07 +0200

    fix cors error

--- Sharkspotter-Backend/Program.cs
+++ Sharkspotter-Backend/Program.cs
@@ -59,7 +59,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors(x => x
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
