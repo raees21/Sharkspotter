968c8d12808383ce94d8327740ef029c0839f8d3
Emanuel Maluleke <emanuelm@bbd.co.za>
2022-07-13 16:55:01 +0200

    add date of latest spotting to getBeach

--- Sharkspotter-Backend/Data/BeachService.cs
+++ Sharkspotter-Backend/Data/BeachService.cs
@@ -17,18 +17,36 @@ namespace Sharkspotter_Backend.Data
             context.SaveChanges();
             return 1;
         }
-        public IEnumerable<Beach> getAllBeaches()
+        public List<Dictionary<string,string>> getAllBeaches()
         {
             var beaches = context.Beaches.ToList();
             var beaches_list = new List<Beach>();
+            var final_list = new List<Dictionary<string,string>>();
             beaches.ForEach(u => beaches_list.Add(u));
-            return beaches_list;
+            foreach(Beach beach in beaches){
+                var map = new Dictionary<string, string>();
+                var spotting = context.Spottings.Where(s=>s.beachid == beach.beachid).ToList();
+                DateTime date =new DateTime();
+                if(spotting.Count()>0){
+                    date = spotting[spotting.Count()-1].spottingAt;
+                }
+                
+                map.Add("beachId", beach.beachid.ToString());
+                map.Add("latitude", beach.latitude.ToString());
+                map.Add("longitude", beach.longitude.ToString());
+                map.Add("beach_name", beach.beach_name);
+                map.Add("description", beach.description);
+                if(spotting.Count()>0){
+                    map.Add("date", date.ToString());
+                }                                
+                final_list.Add(map);
+            }
+            return final_list;
         }
 
         public async Task<Beach> getBeach(int id)
         {
             Beach beach = await context.Beaches.FindAsync  (id);
-
             return beach;
         }
 
